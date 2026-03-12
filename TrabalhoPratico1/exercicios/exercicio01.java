e5b420a14e238b01cefd7a9bfb95e40a7b9c1697
Leonardo <leokenjits@gmail.com>
2026-03-12 20:31:03 -0300

    Adding package exercises

---
+++ TrabalhoPratico1/exercicios/exercicio01.java
@@ -0,0 +1,24 @@
+package exercicios;
+/******************************************************************************
+ Curso: Engenharia de Software
+ Disciplina: Análise e Projeto Orienta a Objetos
+ Professor: José Carlos Flores
+ Turma: ESOFT3A
+ Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 24001310-2 - Jonathan de Lima Bellato
+ 25127107-2 - Tiago Joslin Ferreira Mendes
+ 25011023-2 - Lucas Coelho Suero
+ Data: 05 de Março de 2026
+ Descritivo: Escreva um programa que calcule a soma de dois números
+*******************************************************************************/
+public class exercicio01 {
+
+	public static void main(String[] args) {
+	
+	int var1 = 1;
+	int var2 = 68;
+	
+	System.out.println(var1+var2);
+	}
+}
