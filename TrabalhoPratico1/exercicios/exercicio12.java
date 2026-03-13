4ac68f7dcd1694384bf1d32c06ee84330388a991
Leonardo <leokenjits@gmail.com>
2026-03-12 22:38:41 -0300

    Add files via upload

---
+++ TrabalhoPratico1/exercicios/exercicio12.java
@@ -0,0 +1,31 @@
+package exercicios;
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Análise e Projeto Orienta a Objetos
+Professor: José Carlos Flores
+Turma: ESOFT3A
+Componentes:
+25185655-2 - Leonardo Kenji Tanida Soares
+24001310-2 - Jonathan de Lima Bellato
+25127107-2 - Tiago Joslin Ferreira Mendes
+25011023-2 - Lucas Coelho Suero
+26008977-2 - Pedro Augusto Cordeiro Holanda
+Data: 12 de Março de 2026
+Descritivo: Desenvolva um jogo onde o computador sorteie um número entre 1 e 100, e o usuário tente adivinhá-lo.
+O programa deve dar dicas ("maior" ou "menor") até o acerto.
+*******************************************************************************/
+public class exercicio12 {
+	
+	public class Aluno extends Pessoa{
+		public Aluno(int idade, String nome) {
+			super(idade, nome);
+			
+		}
+		String curso;
+		int matricula;
+	
+		}
+		
+	}
+
+
---
+++ TrabalhoPratico1/exercicios/exercicio17.java
@@ -0,0 +1,40 @@
+package exercicios;
+import java.util.Random;
+import java.util.Scanner;
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Análise e Projeto Orienta a Objetos
+Professor: José Carlos Flores
+Turma: ESOFT3A
+Componentes:
+25185655-2 - Leonardo Kenji Tanida Soares
+24001310-2 - Jonathan de Lima Bellato
+25127107-2 - Tiago Joslin Ferreira Mendes
+25011023-2 - Lucas Coelho Suero
+26008977-2 - Pedro Augusto Cordeiro Holanda
+Data: 12 de Março de 2026
+Descritivo: Escreva um programa que leia 5 números do usuário e os armazene em um array, depois exiba todos os
+números.
+*******************************************************************************/
+public class exercicio17 {
+	
+	public static void main(String[] args) {
+		Random randomizador = new Random();
+		Scanner sc = new Scanner(System.in);
+		int segredo = randomizador.nextInt(100);
+		
+		while(true){
+			System.out.println("Digite um número:");
+			int guess = sc.nextInt();
+			if (guess < segredo) {
+				System.out.println("maior");
+			}else if(guess > segredo){
+				System.out.println("menor");
+			}else {
+				break;
+			}
+		}
+		System.out.println("Parabéns! O número era " + segredo);
+	}
+
+}
